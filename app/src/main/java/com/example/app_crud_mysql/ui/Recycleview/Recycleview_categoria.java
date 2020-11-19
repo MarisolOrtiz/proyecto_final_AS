84d593720dae70dae3aa8329dd5730e4e7fae5bb
MarisolOrtiz <64797672+MarisolOrtiz@users.noreply.github.com>
2020-11-19 15:33:19 -0600

    Hecho!

---
+++ app/src/main/java/com/example/app_crud_mysql/ui/Recycleview/Recycleview_categoria.java
@@ -0,0 +1,18 @@
+package com.example.app_crud_mysql.ui.Recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.app_crud_mysql.R;
+
+public class Recycleview_categoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycleview_categoria2);
+
+
+    }
+}
